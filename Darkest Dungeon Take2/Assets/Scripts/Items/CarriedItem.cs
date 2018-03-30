4ced37c6b1d65a0ae39d3043a86d15958df51465
DubstepHeld <samuel.barthold@gmail.com>
2018-03-30 17:38:01 +0200

    30_03_18
    items!!!

---
+++ Darkest Dungeon Take2/Assets/Scripts/Items/CarriedItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarriedItem : MonoBehaviour {
+
+    public Items items;
+
+    public string weaponName;
+    public int weaponDamage;
+    public bool weaponEquipped = false;
+    public int eWeaponTier = 1;
+
+	void Start () {
+        UpdateWeapon();
+	}
+
+    public void UpdateWeapon() {
+        if (weaponEquipped == true) {
+            items.SetWeapon(eWeaponTier);
+            weaponName = items.name;
+            weaponDamage = items.damage;
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ Darkest Dungeon Take2/Assets/Scripts/Items/Items.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour {
+
+    public Dice dice;
+
+    public string name = "banana";
+    public int damage;
+    
+    public int tier1_minDamage = 1;
+    public int tier1_maxDamage = 3;
+
+    public int tier2_minDamage = 3;
+    public int tier2_maxDamage = 5;
+
+    public int tier3_minDamage = 5;
+    public int tier3_maxDamage = 7;
+
+    public void SetWeapon(int tier) {
+        switch (tier) {
+            case 1:
+                damage = dice.RollDice(tier1_minDamage, tier1_maxDamage);
+                Debug.Log("Tier 1 Weapon set!");
+                break;
+            case 2:
+                damage = dice.RollDice(tier2_minDamage, tier2_maxDamage);
+                Debug.Log("Tier 2 Weapon set!");
+                break;
+            case 3:
+                damage = dice.RollDice(tier3_minDamage, tier3_maxDamage);
+                Debug.Log("Tier 3 Weapon set!");
+                break;
+        }
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
